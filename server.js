e2da0e350d8d6062d1205e68175238ff3b58a09c
nettiliike <liikenetti@gmail.com>
2026-05-02 21:41:56 +0300

    Support Firebase key from environment

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const cors = require("cors");
 const session = require("express-session");
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json");
+let serviceAccount;
+
+if (process.env.FIREBASE_KEY) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+} else {
+  serviceAccount = require("./serviceAccountKey.json");
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
